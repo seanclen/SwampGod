85618e2ddf901deaef46e70e49a68d5dca7fdd5f
Kate Travers <ktravers@udel.edu>
2015-11-15 14:10:40 -0500

    bounds for TrashCan

--- src/swampgod/TrashCan.java
+++ src/swampgod/TrashCan.java
@@ -5,6 +5,8 @@ package swampgod;
  */
 
 import java.awt.Point;
+import java.awt.Rectangle;
+
 
 public class TrashCan implements java.io.Serializable{
 	/**
@@ -13,14 +15,23 @@ public class TrashCan implements java.io.Serializable{
 	private static final long serialVersionUID = 1152015;
 	Point Position;
 	int radius;
-	
+	Rectangle bounds = new Rectangle();
 	
 	/**
 	 * Constructs a trash can with a position and radius
 	 */
 	public TrashCan(){
 		Position.setLocation(100, 500);
+		setBounds(Position.x, Position.y, 200, 200);
+		
 		radius =200;
+	}
+	public void setBounds(int x, int y, int width, int height) {
+		this.bounds = new Rectangle(x, y, width, height);
+	}
+	
+	public Rectangle getBounds() {
+		return this.bounds;
 	}
 }
 
